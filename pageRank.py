686dcf9087ec350bb02d987f6cb507a9452b9611
Hampus-Lindgren <hamlindg@kth.se>
2026-03-08 12:46:04 +0100

    Remove NetworkX and use GPU

--- pageRank.py
+++ pageRank.py
@@ -1,5 +1,47 @@
 import operator, sys, cProfile, networkx as nx
-from utils import parse
+from utils import parse, parseOriginal
+from collections import defaultdict
+import cupy as cp
+
+class Graph:
+    #@profile
+    def __init__(self, graph: tuple[dict[str, int], dict[int, str], defaultdict[int, list]], directed: bool):
+
+        self.label_to_id, self.id_to_label, self.outgoing_edges = graph[0], graph[1], graph[2]
+
+        self.V = len(self.label_to_id)
+
+        self.adjacency_matrix = cp.zeros((self.V, self.V), dtype=int)
+        for i in range(self.V):
+            self.adjacency_matrix[i][self.outgoing_edges[i]] = 1
+
+        self.d = 0.85
+        self.directed = directed
+        self.const = ((1 - self.d) * (1/float(self.V)))
+        
+        self.outlinks = cp.array([len(self.outgoing_edges[i]) for i in range(self.V)]) # number of outgoing links from each vertex
+        self.ranks = cp.ones(self.V) / float(self.V)
+
+    
+    #@profile
+    def rank(self):
+        for _ in range(10):
+            # compute rank / outlinks for all vertices
+            update_factors = self.ranks / self.outlinks
+            update_factors = cp.where(cp.isinf(update_factors), 0, update_factors) # fix cases where the number of outlinks is zero
+
+            rank_sums = self.const + self.d * (self.adjacency_matrix @ update_factors)
+            self.ranks = rank_sums
+
+            """for node in range(self.V): # iterate over all nodes
+                current_outgoing = self.outgoing_edges[node] # get adjacency list of node
+                rank_sum = cp.nansum(update_factors[current_outgoing]) # sum values of all adjacent nodes
+
+                self.ranks[node] = self.const + self.d * rank_sum # update ranks with new values
+            
+            print(rank_sums[0:32] - self.ranks[0:32])"""
+
+       # return p
 
 class PageRank:
     #@profile
@@ -20,15 +62,15 @@ class PageRank:
         else:
             self.degrees = dict(self.graph.degree)
     
-    @profile
+    #@profile
     def rank(self):
         # it doesn't seem like there's any point in distinguishing between the directed and undirected case here, 
         # it just takes more time for the same result
         for key, _ in self.graph.nodes(data=True):
             self.ranks[key] = 1/float(self.V)
         
-        for _ in range(10): # Why does it run 10 times?
-            for key, node in self.graph.nodes(data=True):
+        for _ in range(10):
+            for key, node in self.graph.nodes(data=True): # iterate over all nodes
                 rank_sum = 0
 
                 if self.directed: # Graph is directed
@@ -37,8 +79,7 @@ class PageRank:
                             rank_sum += self.ranks[n] / outlinks
 
                 else: # Graph is undirected
-                    neighbors = self.graph[key]
-                    for n in neighbors:
+                    for n in self.graph[key]:
                         if self.ranks[n] is not None:
                             outlinks = self.degrees[n]
                             rank_sum += self.ranks[n] / outlinks
@@ -53,13 +94,14 @@ if __name__ == '__main__':
         print('Expected input format: python pageRank.py <data_filename> <directed OR undirected>')
     else:
         filename = sys.argv[1]
-        isDirected = False
-        if sys.argv[2] == 'directed':
-            isDirected = True
+        isDirected = sys.argv[2] == 'directed'
 
         graph = parse(filename, isDirected)
-        p = PageRank(graph, isDirected)
+        p = Graph(graph, isDirected)
         p.rank()
-        sorted_r = sorted(p.ranks.items(), key=operator.itemgetter(1), reverse=True)
-        for tup in sorted_r:
+
+        sorted_ranks = sorted([(p.id_to_label[i], p.ranks[i]) for i in range(p.V)], key=operator.itemgetter(1), reverse=True)
+
+        #sorted_ranks = sorted(p.ranks.items(), key=operator.itemgetter(1), reverse=True)
+        for tup in sorted_ranks:
             print('{0:30} :{1:10}'.format(str(tup[0]), tup[1]))
--- utils.py
+++ utils.py
@@ -1,34 +1,59 @@
-import re, csv, networkx as nx
+import csv, networkx as nx
 import cython_string
+from collections import defaultdict
 
-@profile
-def parse(filename, isDirected):
+#@profile
+def parseOriginal(filename, isDirected):
     with open(filename, 'r') as file:
         reader = csv.reader(file, delimiter=',')
         data = [row for row in reader]
         file.close()
-    #print("Reading and parsing the data into memory...")
     if isDirected:
         return parse_directed(data)
     else:
         return parse_undirected(data)
 
+#@profile
+def parse(filename, isDirected):
 
-@profile
+    label_to_id: dict[str, int] = {}
+    id_to_label: dict[int, str] = {}
+    outlinks: defaultdict[int, list] = defaultdict(list)
+    next_id = [0]
+
+    def get_or_create_id(label: str, next_id: list) -> int:
+        if label not in label_to_id:
+            label_to_id[label] = next_id[0]
+            id_to_label[next_id[0]] = label
+            next_id[0] += 1
+        return label_to_id[label]
+
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        for row in reader:
+            src_label = row[0]
+            dst_label = row[2]
+
+            src_id = get_or_create_id(src_label, next_id)
+            dst_id = get_or_create_id(dst_label, next_id)
+
+            outlinks[src_id].append(dst_id)
+            if not isDirected:
+                outlinks[dst_id].append(src_id)
+    
+    return (label_to_id, id_to_label, outlinks)
+
+
+#@profile
 def parse_undirected(data):
     G = nx.Graph()
-    #nodes = set([row[0] for row in data])
     edges = [(row[0], row[2]) for row in data]
-
-    #num_nodes = len(nodes)
-    #rank = 1/float(num_nodes)
-    #G.add_nodes_from(nodes, rank=rank)
     G.add_edges_from(edges)
 
     return G
 
 
-@profile
+#@profile
 def parse_directed(data):
     DG = nx.DiGraph()
 
@@ -39,26 +64,26 @@ def parse_directed(data):
         val_a = cython_string.digits(row[1])
         val_b = cython_string.digits(row[3])
 
-        #DG.add_edge(node_a, node_b) # wtf is the purpose of this line
+        if val_a >= val_b:
+            DG.add_edge(node_a, node_b)
+        else:
+            DG.add_edge(node_b, node_a)
+
+    return DG
+
+def parse_directed2(data):
+    DG = nx.DiGraph()
+
+    for row in data:
+
+        node_a = cython_string.format_key(row[0])
+        node_b = cython_string.format_key(row[2])
+        val_a = cython_string.digits(row[1])
+        val_b = cython_string.digits(row[3])
+
         if val_a >= val_b:
             nx.add_path(DG, [node_a, node_b])
         else:
             nx.add_path(DG, [node_b, node_a])
 
     return DG
-
-
-def digits(val):
-    return int(re.sub(r"\D", "", val))
-
-
-def format_key(key: str):
-    key = key.strip() 
-    if key.startswith('"') and key.endswith('"'):
-        key = key[1:-1]
-    return key 
-
-
-def print_results(f, method, results):
-    print(method)
-
