6eeb9bb43922ef317102f2929a02187b65db5fe8
Hampus-Lindgren <hamlindg@kth.se>
2026-03-12 19:54:56 +0100

    Add validation test

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-# TODO: Set up unit tests for comparing pageRank to pageRank_unmodified
+import pageRank
+import pageRank_baseline
+import pytest
+
+@pytest.mark.parametrize('dataset', ["airplaneRoutes", "stateborders", "redditHyperlinks"])
+def test_equals_baseline(dataset):
+	filename = f".\\data\\{dataset}.csv"
+	results_opt = pageRank.run(filename, True, 10)
+	results_baseline = pageRank_baseline.run(filename, True)
+	for i in range(len(results_baseline)):
+		# labels can be different (appear in different order) if two nodes have identical rank due to being functionally identical (e.g. having no inward links)
+		assert results_baseline[i][1] == pytest.approx(results_opt[i][1]) # approx accounts for SLIGHT implementation-based differences
